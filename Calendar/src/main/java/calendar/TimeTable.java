1625b1bb51fbae264bb8729c4191905064d8033b
MagicalEight <matthew_ruder@outlook.com>
2018-01-23 09:17:59 -0800

    Imported Bugs
    Bugs from assignment 1 imported.

--- Calendar/src/main/java/calendar/TimeTable.java
+++ Calendar/src/main/java/calendar/TimeTable.java
@@ -48,6 +48,10 @@ public class TimeTable {
 	            calDays.add(new CalDay(nextDay));
 	            nextDay.add(nextDay.DAY_OF_MONTH, 1);
 	        }
+	        while (nextDay.equals(lastDay)) {
+	        	calDays.add(new CalDay(nextDay));
+	            nextDay.add(nextDay.DAY_OF_MONTH, 1);
+	        }
 	        
 	        //Retrieve the appts - <appt> 
 		for (int i = 0; i < appts.size(); i++) {
