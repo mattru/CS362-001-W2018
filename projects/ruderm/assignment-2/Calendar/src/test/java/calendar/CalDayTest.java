acf57f7d255c23fd4824628de59efdf367c44f63
MagicalEight <matthew_ruder@outlook.com>
2018-01-28 23:27:54 -0800

    Added Test Classes

--- projects/ruderm/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
+++ projects/ruderm/assignment-2/Calendar/src/test/java/calendar/CalDayTest.java
@@ -5,7 +5,7 @@ package calendar;
  */
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-
+import java.util.LinkedList;
 
 import org.junit.Test;
 
@@ -15,11 +15,27 @@ public class CalDayTest {
 
 	 @Test
 	  public void test01()  throws Throwable  {
-
+		 int startHour=21;
+		 int startMinute=30;
+		 int startDay=15;
+		 int startMonth=01;
+		 int startYear=2018;
+		 String title="Birthday Party";
+		 String description="This is my birthday party.";
+		 //Construct a new Appointment object with the initial data	 
+		 Appt appt = new Appt(startHour,
+		          startMinute ,
+		          startDay ,
+		          startMonth ,
+		          startYear ,
+		          title,
+		         description);
+		 GregorianCalendar cal = new GregorianCalendar(2018, 1, 15);
+		 CalDay testDays = new CalDay(cal);
+		 testDays.addAppt(appt);
+		 //assertions
+		 assertEquals(appt.toString(),testDays.getAppts().getFirst().toString());
+		 CalDay noDays = new CalDay(); assertFalse(noDays.isValid());
+		 assert(testDays.toString() != null);
 	 }
-	 @Test
-	  public void test02()  throws Throwable  {
-		 
-	 }
-//add more unit tests as you needed	
 }
